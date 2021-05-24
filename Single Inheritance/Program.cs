2b4b0b5c249d79362265e598431399fdb0e2679c
Rohitprabha <67499119+Rohitprabha@users.noreply.github.com>
2021-05-24 16:06:52 +0530

    Add files via upload

---
+++ Single Inheritance/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Single_Inheritance
+{
+    public class Animal
+    {
+        public void eat()
+        {
+            Console.WriteLine("Eating..."); 
+        }
+    }
+    public class Dog : Animal
+    {
+        public void bark() 
+        {
+            Console.WriteLine("Barking..."); 
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Dog d1 = new Dog();
+            d1.eat();
+            d1.bark();
+        }
+    }
+}
