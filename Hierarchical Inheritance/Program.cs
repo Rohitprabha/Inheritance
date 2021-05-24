89a0ae746a08a23f5b45193ad79b1de217bb3bb4
Rohitprabha <67499119+Rohitprabha@users.noreply.github.com>
2021-05-24 16:10:20 +0530

    Add files via upload

---
+++ Hierarchical Inheritance/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace Hierarchical_Inheritance
+{
+    class Father
+    {
+        public void display()
+        {
+            Console.WriteLine("Father");
+        }
+    }
+    class Son : Father
+    {
+        public void display1()
+        {
+            Console.WriteLine("son");
+        }
+    }
+    class Daughter : Father
+    {
+        public void display2()
+        {
+            Console.WriteLine("Daughter");
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Father f = new Father();
+            f.display();
+            Son s = new Son();
+            s.display();
+            s.display1();
+            Daughter d = new Daughter();
+            d.display();
+            d.display2();
+        }
+    }
+}
