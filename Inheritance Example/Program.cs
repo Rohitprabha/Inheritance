c50df162ee319b38b73a128fc8f5a4dd2f1e200d
Rohitprabha <67499119+Rohitprabha@users.noreply.github.com>
2021-05-24 16:04:35 +0530

    Add files via upload

---
+++ Inheritance Example/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Inheritance_Example
+{
+    public class ParentClass
+    {
+        public ParentClass()
+        {
+            Console.WriteLine("ParentClass Constructor called");
+        }
+    }
+    public class ChildClass : ParentClass
+    {
+        public ChildClass()
+        {
+            Console.WriteLine("ChildClass Constructor called");
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ChildClass cc = new ChildClass();
+        }
+    }
+}
